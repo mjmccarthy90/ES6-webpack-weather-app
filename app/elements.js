013e1233ccc71b2f3ab949b12362fd8f3ffcbe57
Michael McCarthy <michael.j.mccarthy90@gmail.com>
2019-11-20 16:30:46 -0600

    ES6 Weather App

---
+++ app/elements.js
@@ -0,0 +1,8 @@
+export const ELEMENT_SEARCH_BUTTON = document.querySelector('button');
+export const ELEMENT_SEARCHED_CITY = document.querySelector('#city');
+
+export const ELEMENT_LOADING_TEXT = document.querySelector('#load');
+export const ELEMENT_WEATER_BOX = document.querySelector('#weather');
+export const ELEMENT_WEATHER_CITY = document.querySelector('#weatherCity');
+export const ELEMENT_WEATHER_DESC = document.querySelector('#weatherDescription');
+export const ELEMENT_WEATHER_TEMP = document.querySelector('#weatherTemperature');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry:["./app/main.js"],
+  output: {
+    filename:"bundle.js",
+  },
+  module: {
+    rules: [ {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
